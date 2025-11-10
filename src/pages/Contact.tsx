9eddc84ddd9a8c9e53e254b806660934831f10d7
Zivan34 <keremcemtorun@gmail.com>
2025-11-10 23:20:27 +0100

    update package.json for deployment scripts and add gh-pages dependency; remove unused Card import in Contact component

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import "../styles/About.css";
 
 export default function Contact() {
